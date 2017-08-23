1101e40b1ae9c83f8afc88dd52e8a8f199bc6912
EvanGee <evangiese77@gmail.com>
2017-08-22 19:36:23 -0600

    asdf

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,6 @@
 const logger = require('../lib/logger')
 
 logger.info('Starting server...')
-require('../../server/main').listen(process.env.PORT || 3000, () => {
+require('../../server/main').listen(process.env.PORT || 3000, process.env.YOUR_HOST || '0.0.0.0', () => {
   logger.success('Server is running at port ' + process.env.PORT || 3000)
 })
